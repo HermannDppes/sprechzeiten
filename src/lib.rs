e30b21f8ee6fdb3740547bba30c7b72224911460
Hermann Döppes <hermannd.ouml.ppes@gmail.com>
2018-09-28 17:10:22 +0200

    Some types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,106 @@
 pub fn process(str: &str) -> &str {
 	str
 }
+
+use std::collections::HashSet;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+enum Day {
+	Mo,
+	Di,
+	Mi,
+	Do,
+	Fr
+}
+
+impl Day {
+	// TODO: Is there a sensible trait I should implement instead?
+	fn next(&self) -> Day {
+		match self {
+			Day::Mo => Day::Di,
+			Day::Di => Day::Mi,
+			Day::Mi => Day::Do,
+			Day::Do => Day::Fr,
+			Day::Fr => panic!()
+		}
+	}
+}
+
+struct Days {
+	days: HashSet<Day>
+}
+
+impl Days {
+	fn new() -> Days {
+		let days = HashSet::new();
+		Days {days}
+	}
+
+	fn insert(&mut self, begin: Day, end: Day) {
+		self.days.insert(begin.clone());
+		let mut day = begin;
+		while day != end {
+			day = day.next();
+			self.days.insert(day.clone());
+		}
+	}
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Time {
+	hours: u8,
+	minutes: u8
+}
+
+impl Time {
+	fn new(hours: u8, minutes: u8) -> Time {
+		assert!(hours <= 23);
+		assert!(minutes <= 59);
+		Time {hours, minutes}
+	}
+}
+
+#[derive(Debug)]
+struct TimeRange {
+	begin: Time,
+	end: Time
+}
+
+impl TimeRange {
+	fn new(begin: Time, end: Time) -> TimeRange {
+		TimeRange {begin, end}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn after_monday() {
+		assert_eq!(Day::Mo.next(), Day::Di);
+	}
+
+	#[test]
+	#[should_panic]
+	fn after_friday() {
+		Day::Fr.next();
+	}
+
+	#[test]
+	fn time_works() {
+		assert_eq!(Time::new(11, 23), Time {hours: 11, minutes: 23});
+	}
+
+	#[test]
+	#[should_panic]
+	fn time_fail_hours() {
+		Time::new(24, 13);
+	}
+
+	#[test]
+	#[should_panic]
+	fn time_fail_minutes() {
+		Time::new(20, 60);
+	}
+}
